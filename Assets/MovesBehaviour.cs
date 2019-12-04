2d992537f98d1b091da609b9ca43db56a3395c4b
elias174 <elias.valencia@ucsp.edu.pe>
2019-12-04 00:02:23 -0500

    Restart scene

--- Assets/MovesBehaviour.cs
+++ Assets/MovesBehaviour.cs
@@ -5,14 +5,17 @@ using UnityEngine.UI;
 using UnityEngine;
 using System;
 using System.Linq;
+using UnityEngine.SceneManagement;
 
 
 public class MovesBehaviour : MonoBehaviour
 {
     public GameObject item;
     public GameObject itemPossible;
+    public GameObject prefabButton;
     public string source;
     public List<Tuple<string, string>> moves = new List<Tuple<string, string>>();
+    public List<Move> official_moves = new List<Move>();
 
     public void makeMove()
     {
@@ -64,18 +67,34 @@ public class MovesBehaviour : MonoBehaviour
         }
     }
 
+    public void restart_board_with_official_moves()
+    {
+        official_moves.Reverse();
+        StartCoroutine(simulate_moves(official_moves, false));
+        official_moves.Reverse();
+    }
+
     public void DoMoves(List<Move> moves, bool slow_motion)
     {
+        Debug.Log("Tryying do moooves");
         StartCoroutine(simulate_moves(moves, slow_motion));
     }
     IEnumerator simulate_moves(List<Move> moves, bool slow_motion)
     {
         foreach (Move move in Enumerable.Reverse(moves))
         {
+            Debug.Log(move.source_position);
             PlayerType type_a = GameManager.Instance.CurrentPlayer.Type;
             GameManager.Instance.SimulateMove(move.source_position, move.target_position);
             while (type_a == GameManager.Instance.CurrentPlayer.Type) { yield return null; }
-            if(slow_motion) yield return new WaitForSeconds(2);
+            if(slow_motion) yield return new WaitForSeconds(1);
+        }
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        foreach (Move move in official_moves)
+        {
+            PlayerType type_a = GameManager.Instance.CurrentPlayer.Type;
+            GameManager.Instance.SimulateMove(move.source_position, move.target_position);
+            while (type_a == GameManager.Instance.CurrentPlayer.Type) { yield return null; }
         }
     }
 
@@ -83,7 +102,6 @@ public class MovesBehaviour : MonoBehaviour
     void Start()
     {
         Debug.Log("Currently on Game Scene");
-        Debug.Log(GlobalVars.player_id);
         RestClient.GetArray<Move>("https://ihc-chess-server.herokuapp.com/list-moves/"+GlobalVars.player_current_game).Then(response => {
             for (int i = 0; i < response.Length; i++)
             {
@@ -108,15 +126,14 @@ public class MovesBehaviour : MonoBehaviour
                 newitem.GetComponent<RectTransform>().SetInsetAndSizeFromParentEdge(RectTransform.Edge.Left, 0, 200);
                 newitem.GetComponent<RectTransform>().GetComponentInChildren<Text>().text = string.Format("Propuesta {0}", response[i].id);
 
+                //Button tempButton = newitem.GetComponent<Button>();
                 Button tempButton = newitem.GetComponent<Button>();
-                int tempInt = i;
                 List<Move> moves_received = new List<Move>(new Move[] {});
                 foreach (Move move in response[i].moves)
                 {
                     moves_received.Add(move);
                 }
                 tempButton.onClick.AddListener(() => DoMoves(moves_received, true));
-
 
                 newitem.transform.Find("possiblemovesId").GetComponent<Text>().text = response[i].id.ToString();
 
